8fea8c2dc353dcf2abcaa17d3fbde1a0893aea39
Vadim Merenovskiy <101852782+RunninFridge@users.noreply.github.com>
2022-10-31 19:10:01 -0700

    created testing schema sql file

--- db/schema.sql
+++ db/schema.sql
@@ -3,13 +3,24 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-
-)
+    id INT AUTO_INCREMENT NOT NULL,
+    department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
 
 CREATE TABLE title (
-
-)
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY(id)
+);
 
 CREATE TABLE employee (
-
-)
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(40) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    PRIMARY KEY(id)
+);
