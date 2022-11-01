d0385f1ac2f5046ed38e0515db5e47f0fe8dc6b6
Vadim Merenovskiy <101852782+RunninFridge@users.noreply.github.com>
2022-10-31 21:07:55 -0700

    added new employee prompt

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const ConsoleTable = require('console.table');
 const util = require('util');
 const chalk = require('chalk'); //Found on google to assist with prompts
 const { createConnection } = require('net');
+const { connect } = require('http2');
 
 //Creating connection woth properties
 let connection = mysql.createConnection({
@@ -18,8 +19,8 @@ connection.connect(err => {
     if (err) throw err;
     prompt();
 });
-
-function prompt() {
+//Prompts at the begining
+function displayPrompts() {
     inquirer
     .prompt({
         type: 'list',
@@ -59,3 +60,76 @@ function prompt() {
             break;
     };
 }
+//Had to get help on views updates and add
+//To view all employees/roles/departmentds
+const viewEmployees = () => {
+    const query = 'SELECT * FROM employee';
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+    })
+    displayPrompts();
+}
+const viewEmployeeRole = () => {
+    const query = 'SELECT * FROM title';
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+    })
+    displayPrompts();
+}
+const viewDepartments = () => {
+    const query = 'SELECT * FROM department';
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+    })
+    displayPrompts();
+}
+
+//Adding employee
+const addEmployee = () => {
+    connection.query('SELECT * FROM title', (err,titles) => {
+        if(err) throw err;
+        titles = titles.map((title) => {
+            return {
+                name: title.title,
+                value: role.id,
+            };
+        });
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'firstName',
+                    message: 'Please enter first name of the new Employee'
+                },
+                {
+                    type: 'input',
+                    name: 'lastName',
+                    message: 'Please enter last name of the new Employee'
+                },
+                {
+                    type: 'input',
+                    name: 'role',
+                    message: 'Please enter the role of the new Employee'
+                },
+                {
+                    type: 'list',
+                    name: 'managerID',
+                    message: 'Please select the manager ID'
+                },
+            ])
+            .then((data) => {
+                connection.query('INSERT INTO employee SET',
+                {first_name: data.firstName, 
+                last_name: data.lastName,
+                role_id: data.role, 
+                manager_id: data.managerID},
+                 (err) => {
+                    if (err) throw err;
+                    displayPrompts();
+                });
+            });
+        });
+    };
